b036647f3d820b8bf386e316cca227027aa29ea6
imgopalsingh <131387262+imgopalsingh@users.noreply.github.com>
2023-09-04 15:29:27 +0530

    Add files via upload

---
+++ Average-master/Average-master/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Average
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            float[] values = { 1.2f, 3.4f, 5.4f, 6.5f, 8.6f, 8.9f };
+            float sum = 0;
+            for(int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+            float average = sum / values.Length;
+            Console.WriteLine("the average of the values : " + average);
+            Console.ReadLine();
+        }
+    }
+}
